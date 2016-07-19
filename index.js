363c3ee80189705c64dc58e4dec55511661cca17
John Otander <johnotander@gmail.com>
2016-07-19 01:13:31 -0600

    Add clipboard niceties

--- index.js
+++ index.js
@@ -46,7 +46,44 @@ electron.app.on('ready', () => {
           if (focusedWindow) focusedWindow.toggleDevTools()
         }
       }]
-    }
+    },
+  {
+    label: 'Edit',
+    submenu: [
+      {
+        label: 'Undo',
+        accelerator: 'CmdOrCtrl+Z',
+        role: 'undo'
+      },
+      {
+        label: 'Redo',
+        accelerator: 'Shift+CmdOrCtrl+Z',
+        role: 'redo'
+      },
+      {
+        type: 'separator'
+      },
+      {
+        label: 'Cut',
+        accelerator: 'CmdOrCtrl+X',
+        role: 'cut'
+      },
+      {
+        label: 'Copy',
+        accelerator: 'CmdOrCtrl+C',
+        role: 'copy'
+      },
+      {
+        label: 'Paste',
+        accelerator: 'CmdOrCtrl+V',
+        role: 'paste'
+      },
+      {
+        label: 'Select All',
+        accelerator: 'CmdOrCtrl+A',
+        role: 'selectall'
+      }
+    ]}
   ]))
 
   const defUrl = path.join('file://', __dirname, 'index.html')
